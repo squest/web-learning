c89c62724330263a1eb4f620fbb4faceb1365d68
Sabda PS <sabdaps@gmail.com>
2023-03-04 21:16:26 +0700

    trying out reitit

--- ring-alfa/project.clj
+++ ring-alfa/project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.immutant/web "2.1.10"]
+                 [metosin/reitit "0.6.0"]
+                 [ring/ring-defaults "0.3.4"]]
   :main ^:skip-aot ring-alfa.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- ring-alfa/src/ring_alfa/core.clj
+++ ring-alfa/src/ring_alfa/core.clj
@@ -1,7 +1,46 @@
 (ns ring-alfa.core
-  (:gen-class))
+  (:require
+    [immutant.web :as web]
+    [reitit.ring :as ring]
+    [reitit.core :as reitit]
+    [ring.middleware.defaults :refer :all]
+    [ring.middleware.params :refer :all]
+    [immutant.util :as log]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn config []
+  {:port 4000
+   :path "/"
+   :host "localhost"})
+
+(defn handler [_]
+  {:status 200
+   :body "Hello World!"})
+
+(defn routes []
+  (reitit/router
+    [["/" {:get handler}]
+     ["/ping" {:get (fn [_] {:status 200 :body "pong"})}]]))
+
+(defonce system (atom nil))
+
+(defn server-setup []
+  (let [site-config (assoc-in site-defaults [:security :anti-forgery] false)]
+    (-> (wrap-params (routes))
+        (wrap-defaults site-config))))
+
+(defn server-bare []
+  (web/run (ring/ring-handler (routes)) (config)))
+
+(defn server-start []
+  (reset! system (web/run (server-setup) (config))))
+
+(defn server-stop []
+  (web/stop @system))
+
+
+
+
+
+
+
+
